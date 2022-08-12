b7cacb8e7320d400870d8cb1464319ece4dbe748
Sam Apostel <sam@apostel.be>
2022-08-12 12:56:14 +0200

    add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,28 @@
 import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
 import Layout from '../components/Layout/Layout';
+import { GoogleAnalytics, usePageViews, event } from 'nextjs-google-analytics';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	);
+export function reportWebVitals({ id, name, label, value }: NextWebVitalsMetric) {
+	event(name, {
+		category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+		value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+		label: id, // id unique to current page load
+		nonInteraction: true, // avoids affecting bounce rate.
+	});
 }
 
-export default MyApp
+const App = ({ Component, pageProps }: AppProps) => {
+	usePageViews();
+
+	return (
+		<>
+			<GoogleAnalytics />
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
+	);
+};
+
+export default App
