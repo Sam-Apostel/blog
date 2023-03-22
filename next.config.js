5f3050ab2f2c5a23fb284ae11bb3aa85883cfe56
Sam Apostel <sam@apostel.be>
2023-03-22 19:38:30 +0100

    remove superjson fix typescript to below v5 while packages catch up.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
 	swcMinify: true,
 	experimental: {
 		appDir: true,
-		swcPlugins: [['next-superjson-plugin', {}]],
 		serverComponentsExternalPackages: ['@xata.io/client'],
 	},
 };
