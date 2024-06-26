c0aad1bc36b4d8437f534a9980f6019acb3e0797
Sam Apostel <sam@apostel.be>
2024-06-26 18:39:41 +0200

    simplify project card links

--- components/Project/Card.tsx
+++ components/Project/Card.tsx
@@ -1,61 +1,36 @@
 import styles from './Card.module.scss';
 import Link from 'next/link';
 import { Project } from '~/globals/db';
-import LinkIcon from '../LinkIcon';
+import LinkIcon from '../icons/LinkIcon';
 
-type Props = Project
+type Props = Project;
 
 const Card = ({ name, description, slug, url, showUrl, dedicatedPage }: Props) => {
 	return (
 		<article className={styles.article} aria-describedby={`${slug}_hook`}>
-			{url && (showUrl 
-					? <a className={styles.projectLink} href={url}>{url.replace('https://', '')}</a> 
-					: <a className={styles.projectLink} href={url}>view<LinkIcon /></a>
+			<div className={styles.header}>
+				<h3>{name}</h3>
+				{url &&
+					(showUrl ? (
+						<a className={styles.projectLink} href={url}>
+							{url.replace('https://', '')}
+						</a>
+					) : (
+						<a className={styles.projectLink} href={url}>
+							view
+							<LinkIcon />
+						</a>
+					))}
+
+				{dedicatedPage && (
+					<Link className={styles.projectLink} href={`/p/${slug}`}>
+						more {' ->'}
+					</Link>
 				)}
-			{dedicatedPage && (<Link  href={`/p/${slug}`}>
-				<Content 
-					name={name}
-					url={url}
-					showUrl={showUrl}
-					slug={slug}
-					description={description}
-				/>
-			</Link>)}
-			{!dedicatedPage && (
-				<Content 
-					name={name}
-					url={url}
-					showUrl={showUrl}
-					slug={slug}
-					description={description}
-				/>
-			)}
-			
-			
+			</div>
+			<p id={`${slug}_hook`}>{description}</p>
 		</article>
 	);
 };
-
-function Content({ name, url, showUrl, slug, description }: { 
-	name: string,
-	url?: string | null,
-	showUrl: boolean,
-	slug: string, 
-	description: string
-}) {
-	return (
-		<>
-			<div className={styles.header}>
-				<h3>{name}</h3>
-				{url && (showUrl 
-				? <span className={styles.projectLinkPlaceholder}>{url.replace('https://', '')}</span> 
-				: <span className={styles.projectLinkPlaceholder}>view<LinkIcon /></span>
-			)}
-				
-			</div>
-			<p id={`${slug}_hook`}>{description}</p>
-		</>
-	)
-}
 
 export default Card;
